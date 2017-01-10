93998f364f128171ee3b2b05860658836c35cfb4
KunAttilaRCC <akun@student.rcc.edu>
2017-01-09 19:37:41 -0800

    Signed-off-by: KunAttilaRCC <akun@student.rcc.edu>

---
+++ Hmwk/Assignment_1/Gaddis_8thEd_Chap2_Prob16_DiamondPattern/Gaddis_8thEd_Chap2_Prob16_DiamondPattern.cpp
@@ -0,0 +1,29 @@
+/* 
+  File:   Gaddis_8thEd_Chap2_Prob16_DiamondPattern
+  Author: Attila Kun
+  Created on January 9, 2017, 19:15 PM
+  Purpose:  Diamond Pattern
+ */
+
+//System Libraries
+#include <iostream>
+
+using namespace std;
+
+//Executable code begins here!!!
+int main(int argc, char** argv) {
+        
+   //Output values
+
+    cout << "\n   *   \n" ;
+    cout << "\n  ***   \n" ;
+    cout << "\n *****   \n" ;
+    cout << "\n*******   \n" ;
+    cout << "\n *****   \n" ;
+    cout << "\n  ***   \n" ;
+    cout << "\n   *   \n" ;
+       
+    //Exit stage right!
+    return 0;
+}
+
---
+++ Hmwk/Assignment_1/Gaddis_8thEd_Chap2_Prob16_DiamondPattern/build/Debug/Cygwin_1-Windows/Gaddis_8thEd_Chap2_Prob16_DiamondPattern.o.d
@@ -0,0 +1,2 @@
+build/Debug/Cygwin_1-Windows/Gaddis_8thEd_Chap2_Prob16_DiamondPattern.o: \
+ Gaddis_8thEd_Chap2_Prob16_DiamondPattern.cpp

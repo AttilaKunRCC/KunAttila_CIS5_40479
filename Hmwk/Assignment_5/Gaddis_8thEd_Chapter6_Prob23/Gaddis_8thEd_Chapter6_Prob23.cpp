f97f8ed7d3a3305b9f2fbfa426ceb39bf2684183
KunAttilaRCC <akun@student.rcc.edu>
2017-02-02 19:10:34 -0800

    Signed-off-by: KunAttilaRCC <akun@student.rcc.edu>

--- Hmwk/Assignment_5/Gaddis_8thEd_Chapter6_Prob23/Gaddis_8thEd_Chapter6_Prob23.cpp
+++ Hmwk/Assignment_5/Gaddis_8thEd_Chapter6_Prob23/Gaddis_8thEd_Chapter6_Prob23.cpp
@@ -10,8 +10,6 @@ that stores a list of all the prime numbers from 1 through 100 in a file.
 #include <iostream>
 #include <iomanip>
 #include <fstream>
-#include <cstring>
-#include <ctime>
 #include <cstdlib>
 using namespace std;
 
@@ -23,26 +21,18 @@ int main(int argc, char** argv)
     int number;    
     isPrime(number); 
     
-    //Read the name of the file
-    const int SIZE=20;
-    char fileName[SIZE];
-    cout<<"Type in a file name to use"<<endl;
-    cin.getline(fileName,SIZE);
+    ofstream outFile;
+    outFile.open("1-100.txt");
     
-    //Fill this file with numbers
-    ofstream output(fileName);
-    int amountOfData=100;
-    for(int i=1;i<amountOfData;i++){
-        cout << 
-    }
-    
-    //Close the file
-    output.close();
+    for(int i=1;i<100;i++){
+        if(isPrime(i)){
+            outFile<<i<<"\t"<<endl;
+        }
+    }//end for loop
     return 0;
 }
-bool isPrime(int number){
-    int i;
-    for(i=2; i<number; i++){
+bool isPrime(int number){    
+    for(int i=2; i<number; i++){
         if(number % i == 0){
             return false;
         }//end inner if

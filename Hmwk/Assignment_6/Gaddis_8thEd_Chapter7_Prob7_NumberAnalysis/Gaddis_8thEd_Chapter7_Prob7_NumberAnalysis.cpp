cef26628d3960c88be1807b3cb73a7f34cdb58e1
KunAttilaRCC <akun@student.rcc.edu>
2017-02-04 20:15:36 -0800

    Signed-off-by: KunAttilaRCC <akun@student.rcc.edu>

---
+++ Hmwk/Assignment_6/Gaddis_8thEd_Chapter7_Prob7_NumberAnalysis/Gaddis_8thEd_Chapter7_Prob7_NumberAnalysis.cpp
@@ -0,0 +1,62 @@
+/* 
+  File:   Gaddis_8thEd_Chapter7_Prob7_NumberAnalysis
+  Author: Attila Kun
+  Created on January 4, 2017, 19:06 PM
+  Purpose:  Number Analysis Program
+Write a program that asks the user for a file name. Assume the file contains a series of
+numbers, each written on a separate line. The program should read the contents of the
+file into an array and then display the following data:
+• The lowest number in the array
+• The highest number in the array
+• The total of the numbers in the array
+• The average of the numbers in the array
+If you have downloaded this book’s source code from the companion Web site, you
+will find a file named numbers.txt in the Chapter 07 folder. You can use the file to
+test the program. (The companion Web site is at www.pearsonhighered.com/gaddis .)
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//Function Prototypes
+
+//Executable code begins here!!!
+int main(int argc, char** argv) {
+    //Declare Variables
+    const int ARRAY_SIZE = 10; // Array size
+    int numbers[ARRAY_SIZE]; // Array with 10 elements
+    int count = 0; // Loop counter variable
+    ifstream inputFile; // Input file stream object
+    
+    // Open the file.
+    inputFile.open("TenNumbers.txt");
+    
+    // Read the numbers from the file into the array.
+    while (count < ARRAY_SIZE && inputFile >> numbers[count])
+    count++;
+    
+    // Close the file.
+    inputFile.close();
+    
+    // Display the numbers read:
+    cout << "The numbers are: ";
+    for (count = 0; count < ARRAY_SIZE; count++)
+            cout << numbers[count] << " ";
+            cout << endl;
+    return 0;
+    }
+
+    //Exit stage right!
+    return 0;
+}
+
+
+
+ 
+ 
+ 
+
+ 
+
+ 

99757c1ee1215f25f88a0f4ccff93d1efc9936f0
KunAttilaRCC <akun@student.rcc.edu>
2017-02-05 14:08:18 -0800

    Signed-off-by: KunAttilaRCC <akun@student.rcc.edu>

--- Hmwk/Assignment_6/Gaddis_8thEd_Chapter7_Prob12_GradeBook/Gaddis_8thEd_Chapter7_Prob12_GradeBook.cpp
+++ Hmwk/Assignment_6/Gaddis_8thEd_Chapter7_Prob12_GradeBook/Gaddis_8thEd_Chapter7_Prob12_GradeBook.cpp
@@ -26,17 +26,90 @@ Input Validation: Do not accept test scores less than 0 or greater than 100.
 using namespace std;
 
 //Function Prototypes
+void getTestScores(float[], int);
+void getNames(char [][STU_LENGTH]);
+
+const int num_students = 5;
+char num_tests = 4;
+const int LETTERS = 5;
+    
 
 //Executable code begins here!!!
 int main(int argc, char** argv) {
-    //Declare Variables
+    //Declare Variables    
+    string names[num_students];
+    char grades[num_students];
+    double scores[num_students][num_tests];
+    
+    int table1[TBL1_ROWS][COLS] = {{1, 2, 3, 4},
+                                  {5, 6, 7, 8},
+                                  {9, 10, 11, 12}};
+    
+    cout << "The contents of table1 are:\n";
+    showArray(table1, TBL1_ROWS);
+    
     
     //Input values
+    const int SIZE = 4;      // Array size
+    double testScores[SIZE], // Array of test scores
+          total,            // Total of the scores
+          lowestScore,      // Lowest test score
+          average;          // Average test score
+
+   // Set up numeric output formatting.
+   cout << fixed << showpoint << setprecision(1);
+
     
     //Process by mapping inputs to outputs
+    for (int num = 0; num < NUM_STUDENTS; num++)
+   {
+       cout << "Enter student's name: ";
+       cin >> names[num];
+       
+       for (int test = 0; test < TESTS; test++)
+       {
+           cout << "Enter the test score: ";
+           cin >> grades[num][test];
+       }
+}
     
     //Output values
+    // Get the test scores from the user.
+    getTestScores(testScores, SIZE);
 
     //Exit stage right!
     return 0;
+}
+void getTestScores(double scores[], int size)
+{
+   // Loop counter
+   int index;
+   
+   // Get each test score.
+   for(index = 0; index <= size - 1; index++)
+   {
+      cout << "Enter test score number " 
+           << (index + 1) << ": ";
+      cin >> scores[index];
+   }
+}
+
+void showArray(const int array[][COLS], int rows)
+{
+   for (int x = 0; x < rows; x++)
+   {
+      for (int y = 0; y < COLS; y++)
+      {
+         cout << setw(4) << array[x][y] << " ";
+      }
+      cout << endl;
+   }
+}
+void getNames(char names[][STU_LENGTH])
+{
+    for(int i=0;i < STU_NUM;i++)
+    {
+        cout << "Please enter student " << i+1 << " name : ";
+        cin.getline(names[i],STU_LENGTH);
+    }    
 }

a23f21dace586f43b8c853b46c1df588daf95a16
KunAttilaRCC <akun@student.rcc.edu>
2017-02-04 19:01:30 -0800

    Signed-off-by: KunAttilaRCC <akun@student.rcc.edu>

--- Hmwk/Assignment_6/Gaddis_8thEd_Chapter7_Prob4_LargerN/Gaddis_8thEd_Chapter7_Prob4_LargerN.cpp
+++ Hmwk/Assignment_6/Gaddis_8thEd_Chapter7_Prob4_LargerN/Gaddis_8thEd_Chapter7_Prob4_LargerN.cpp
@@ -18,26 +18,29 @@ int highest(int[], int, int);
 //Executable code begins here!!!
 int main(int argc, char** argv) {
     //Declare Variables
-    const int SIZE = 10;                                         //array and size
-    int nums[SIZE] = {0 ,5, 10, 15, 20, 25, 30, 35, 40, 45};    //Assume that the array contains integers
-    int n;                                                  //declared number n
-    
-    cout << " The value of 'n': ";
-    cin >> n;
-    
-
-    highest(nums, SIZE, n); // Calling function   
+    const int SIZE = 10;                                                //array and size
+    int arraynums[SIZE] = {0 ,5, 10, 15, 20, 25, 30, 35, 40, 45};    //Assume that the array contains integers
+    int n;                                                          //declared number n
+ 
+    highest(arraynums, SIZE, n); // Calling function   
 
     //Exit stage right!
     return 0;
 }
 
-int highest(int nums[], int SIZE, int n){
-    int highest = nums[0];
+int highest(int arraynums[], int SIZE, int n){
+    cout << " The value of 'n': ";
+    cin >> n;
+    
+    //int highest = arraynums[0];
+    
     for(int count = 0; count < SIZE; count++){
-        if(nums[count]>highest)
-            highest = nums[count];
-    cout << nums[count] << endl;
+        
+        if(arraynums[count]>n)
+            
+            //highest = arraynums[count];
+        
+    cout << arraynums[count] << " " << endl;
     
     }//end for  
     

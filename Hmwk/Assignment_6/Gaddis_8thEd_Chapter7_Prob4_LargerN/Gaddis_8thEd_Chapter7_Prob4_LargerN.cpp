d6e9407dd009a66bf9aac67b0f56de3729e86622
KunAttilaRCC <akun@student.rcc.edu>
2017-02-04 18:53:53 -0800

    Signed-off-by: KunAttilaRCC <akun@student.rcc.edu>

--- Hmwk/Assignment_6/Gaddis_8thEd_Chapter7_Prob4_LargerN/Gaddis_8thEd_Chapter7_Prob4_LargerN.cpp
+++ Hmwk/Assignment_6/Gaddis_8thEd_Chapter7_Prob4_LargerN/Gaddis_8thEd_Chapter7_Prob4_LargerN.cpp
@@ -4,32 +4,41 @@
   Created on January 4, 2017, 15:40 PM
   Purpose:  Larger Than n
 In a program, write a function that accepts three arguments: an array, the size of the
-array, and a number n . Assume that the array contains integers. The function should
-display all of the numbers in the array that are greater than the number n .
+array, and a number n . Assume that the array contains integers. 
+ * The function should display all of the numbers in the array that are greater than the number n .
  */
 
 //System Libraries
 #include <iostream>
 using namespace std;
 
-//Function Prototypes
-void showValue(int[], int); 
+//Function Prototypes 
+int highest(int[], int, int); 
 
 //Executable code begins here!!!
 int main(int argc, char** argv) {
     //Declare Variables
-    const int SIZE = 20;
-    int numbers[SIZE] = {5, 10, 15, 20, 25, 30, 35, 40};
+    const int SIZE = 10;                                         //array and size
+    int nums[SIZE] = {0 ,5, 10, 15, 20, 25, 30, 35, 40, 45};    //Assume that the array contains integers
+    int n;                                                  //declared number n
+    
+    cout << " The value of 'n': ";
+    cin >> n;
+    
 
-    showValue(numbers, SIZE); // Calling function   
+    highest(nums, SIZE, n); // Calling function   
 
     //Exit stage right!
     return 0;
 }
 
-void showValue(int nums[], int size) // Function Definiton
-{
-    for(int index = 0; index < size; index++)
-        cout << nums[index] << " ";
-    cout << endl;
-}
+int highest(int nums[], int SIZE, int n){
+    int highest = nums[0];
+    for(int count = 0; count < SIZE; count++){
+        if(nums[count]>highest)
+            highest = nums[count];
+    cout << nums[count] << endl;
+    
+    }//end for  
+    
+}//end void highest
---
+++ Hmwk/Assignment_6/Gaddis_8thEd_Chapter7_Prob4_LargerN/build/Debug/Cygwin_1-Windows/Gaddis_8thEd_Chapter7_Prob4_LargerN.o.d
@@ -0,0 +1,2 @@
+build/Debug/Cygwin_1-Windows/Gaddis_8thEd_Chapter7_Prob4_LargerN.o: \
+ Gaddis_8thEd_Chapter7_Prob4_LargerN.cpp

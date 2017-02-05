2c4f1a8c2a6ca2a9ce0142a802678fcad0df9162
KunAttilaRCC <akun@student.rcc.edu>
2017-02-04 17:58:27 -0800

    Signed-off-by: KunAttilaRCC <akun@student.rcc.edu>

---
+++ Hmwk/Assignment_6/Gaddis_8thEd_Chapter7_Prob4_LargerN/Gaddis_8thEd_Chapter7_Prob4_LargerN.cpp
@@ -0,0 +1,34 @@
+/* 
+  File:   Gaddis_8thEd_Chapter7_Prob4_LargerN
+  Author: Attila Kun
+  Created on January 4, 2017, 15:40 PM
+  Purpose:  Larger Than n
+In a program, write a function that accepts three arguments: an array, the size of the
+array, and a number n . Assume that the array contains integers. The function should
+display all of the numbers in the array that are greater than the number n .
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//Function Prototypes
+void arrayFunction(int[], int, int); 
+
+//Executable code begins here!!!
+int main(int argc, char** argv) {
+    //Declare Variables
+    int n;
+    cout << "Enter the value: " << endl;
+    cin >> n;
+    
+    
+    //Input values
+    
+    //Process by mapping inputs to outputs
+    
+    //Output values
+
+    //Exit stage right!
+    return 0;
+}

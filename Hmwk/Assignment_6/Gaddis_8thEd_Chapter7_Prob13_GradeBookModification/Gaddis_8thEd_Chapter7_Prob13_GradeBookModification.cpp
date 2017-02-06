107566bd8c28f6f6880bab095e7b33b9fbfaa1d1
KunAttilaRCC <akun@student.rcc.edu>
2017-02-05 22:49:41 -0800

    Signed-off-by: KunAttilaRCC <akun@student.rcc.edu>

--- Hmwk/Assignment_6/Gaddis_8thEd_Chapter7_Prob13_GradeBookModification/Gaddis_8thEd_Chapter7_Prob13_GradeBookModification.cpp
+++ Hmwk/Assignment_6/Gaddis_8thEd_Chapter7_Prob13_GradeBookModification/Gaddis_8thEd_Chapter7_Prob13_GradeBookModification.cpp
@@ -1,5 +1,5 @@
 /* 
-  File:   Gaddis_8thEd_Chapter7_Prob12_GradeBook.cpp
+  File:   Gaddis_8thEd_Chapter7_Prob13_GradeBookModification.cpp
   Author: Attila Kun
   Created on January 5, 2017, 13:15 PM
   Purpose:  Grade Book Modification
@@ -10,20 +10,19 @@ lowest score when determining the test score averages and letter grades.
 //System Libraries
 #include <iostream>
 using namespace std;
-    const int NUM_NAMES = 5;                    //how many occurrences
-    const int NAMESIZE = 11;                    //how long names can be, 10 letters
-    const int NUM_TESTS = 4;                    //how many tests
-    char name[NUM_NAMES][NAMESIZE];             //two-dimensional name array
-    char grade[5];                              //grade letter for each student array
-    float testscore[NUM_NAMES][NUM_TESTS];     //test score for each student
-    float average[5];                          //average and lowest for each student array
-    
-    //function prototype
-    void calcdata(int, float[][NUM_TESTS]);
-    
-    //start of main
+
+//function prototype
+void calcdata(int, float);
+void getLowest(const float, int);
+
+//start of main
 int main(int argc, char** argv) {
-    float lowest, total;
+    float testscore[NUM_NAMES][NUM_TESTS];     //test score for each student
+    char name[NUM_NAMES][NAMESIZE];             //two-dimensional name array
+    const int NUM_NAMES = 5;                    //how many occurrences for names
+    const int NAMESIZE = 11;                    //how long names can be, 10 letters
+    char grade[5];                              //grade letter for each student array
+    const int NUM_TESTS = 4;                    //how many tests per student
     
     cout <<  "Enter the student's name. \n";
     for (int count = 0; count < NUM_NAMES; count++)
@@ -50,14 +49,8 @@ int main(int argc, char** argv) {
     	}
     
     // Get the lowest test score.
-   lowest = getLowest(grade, NUM_TESTS);
+   getLowest(grade, NUM_TESTS);
 
-   // Subtract the lowest score from the total.
-   total -= lowest;
-
-   // Calculate the average. Divide by 3 because
-   // the lowest test score was dropped.
-   average = total / (NAMESIZE - 1);
     //call to function
     calcdata(NUM_NAMES, testscore);
  
@@ -67,6 +60,8 @@ int main(int argc, char** argv) {
 void calcdata(int NUM_NAMES, float testscore[][NUM_TESTS])
     {
     float total, lowest;
+    float average[5];                          //average and lowest for each student array
+    char grade[];
     //get each students average score
     for (int row = 0; row < NUM_NAMES; row++)
     {
@@ -100,10 +95,8 @@ void calcdata(int NUM_NAMES, float testscore[][NUM_TESTS])
            <<" grade: " << grade[i]
            << endl;
 } //end
-float getLowest(const float array[], int size)
-{
-   float lowest;  // To hold the lowest value
-
+void getLowest(const float array[], int size){
+    float lowest; 
    // Get the first array's first element.
    lowest = array[0];
 
@@ -115,6 +108,13 @@ float getLowest(const float array[], int size)
       if (array[count] < lowest)
          lowest = array[count];
    }
+   
+   // Subtract the lowest score from the total.
+   total -= lowest;
+
+   // Calculate the average. Divide by 3 because
+   // the lowest test score was dropped.
+   average = total / (NAMESIZE - 1);
 
    // Return the lowest value.
    return lowest;
